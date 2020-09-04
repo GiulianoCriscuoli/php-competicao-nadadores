c9df375ed7c4194843aceaf9cc2a22356a54ccc6
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-04 00:38:14 -0300

    Determinando a categoria do nadador

--- index.php
+++ index.php
@@ -5,3 +5,38 @@ $categories[] = 'Infantil';
 $categories[] = 'Adolescente';
 $categories[] = 'Adulto';
 
+$name = 'Giuliano';
+$age = 16;
+
+
+if($age >= 6 && $age <= 12) {
+
+    for($i = 0; $i <= count($categories); $i++) {
+
+        if($categories[$i] === 'Infantil') {
+
+            echo "O nadador ".$name." compete na categoria: ".$categories[$i];
+        }
+    }
+
+} else if($age >= 13 && $age <= 18) {
+
+    for($i = 0; $i <= count($categories); $i++) {
+
+        if($categories[$i] === 'Adolescente') {
+
+            echo "O nadador ".$name." compete na categoria: ".$categories[$i];
+
+        }
+    }
+
+} else {
+
+    for($i = 0; $i <= count($categories); $i++) {
+
+        if($categories[$i] === 'Adulto') {
+
+            echo "O nadador ".$name." compete na categoria: ".$categories[$i];
+        }
+    }
+}
